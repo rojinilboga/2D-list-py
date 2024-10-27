e30ca383aa07d5dc1244b1a298c7f036edbf816d
rojinilboga <rojin@ilboga.nl>
2024-10-27 11:54:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#creating a 2D list
+my_list= [[1,2,3,4],
+          [5,6,7,8],
+          [9,10,11,12]]
+
+print(my_list)
+
+#counting the total number of rows
+rows = len(my_list)
+print("The total rows = ", rows)
+
+#counting the total number of columns
+cols = len(my_list[0])
+print("The total columns = ",cols)
+
